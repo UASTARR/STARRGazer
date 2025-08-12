20ff87c60eaf7c2395a61b3b8f1c0b99e1f6675f
Sovereign Shahid <sovereignshahid@gmail.com>
2025-08-12 17:30:29 -0600

    in theory two axis control

--- starr_gazer_hardware_system/main.py
+++ starr_gazer_hardware_system/main.py
@@ -30,26 +30,31 @@ def io_thread():
     js = pg.joystick.Joystick(0)
     js.init()
 
-    motor = GimbalMotor(33, 13, 38)  # example pins change later
-    motor.set_enable(GPIO.LOW)
+    motor_x = GimbalMotor(33, 13, 38)
+    motor_x.set_enable(GPIO.LOW)
+    motor_y = GimbalMotor(32, 11, 39)
+    motor_y.set_enable(GPIO.LOW)
 
     try:
         while True:
             for event in pg.event.get():
                 if event.type == pg.JOYAXISMOTION:
                     print(f"Axis {event.axis}: {event.value}")
-                    print(f"Running {motor.running}")
+                    print(f"Running {motor_x.running}")
                 if event.type == pg.JOYBUTTONUP and event.button == 0:
                     print("Exitting program on trigger press")
                     raise KeyboardInterrupt
             x_axis = js.get_axis(2)
-            motor.move(x_axis)
+            y_axis = js.get_axis(1)
+            motor_x.move(x_axis)
+            motor_y.move(y_axis)
 
     except KeyboardInterrupt:
         pass
     finally:
         print("Motor stopping")
-        motor.stop_pwm()
+        motor_x.stop_pwm()
+        motor_y.stop_pwm()
         try:
             GPIO.cleanup()
         except OSError:  # For some reason cleanup gives us an os error
