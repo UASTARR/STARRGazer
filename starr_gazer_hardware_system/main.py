50d7c71d0e3e49e25ac1818ebd20d0ed21e27d92
Brian Lin <shihhsu1@ualberta.ca>
2025-08-12 18:44:07 -0600

    refactor: move model into main

--- starr_gazer_hardware_system/main.py
+++ starr_gazer_hardware_system/main.py
@@ -10,9 +10,18 @@ import pygame as pg
 import RPi.GPIO as GPIO
 
 from motor import GimbalMotor
-from model import YoloModel
+from ultralytics import YOLO
+import cv2
 
 JOYSTICK = True
+
+def put_text_rect(img, text, pos, scale=0.5, thickness=1, bg_color=(0,0,0), text_color=(255,255,255)):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text_size, _ = cv2.getTextSize(text, font, scale, thickness)
+    text_w, text_h = text_size
+    x, y = pos
+    cv2.rectangle(img, (x, y - text_h - 6), (x + text_w + 4, y + 4), bg_color, -1)
+    cv2.putText(img, text, (x + 2, y - 2), font, scale, text_color, thickness)
 
 
 def joystick(js, motor_x, motor_y):
@@ -47,7 +56,13 @@ def main():
 
     input_mode = "joystick"
     print(f"Input mode: {input_mode}")
-    model_inf = YoloModel()
+    
+    # Initialize the YOLO model
+    model = YOLO("yolo11s.pt")
+
+    # Starts the display
+    cap = cv2.VideoCapture(f'/dev/video{0}', cv2.CAP_V4L2)
+    prev_time = 0
 
     try:
         while True:
@@ -67,15 +82,38 @@ def main():
                     else:
                         input_mode = "joystick"
                         print("Switching to joystick mode")
+
+            
+            et, img = cap.read()
             if input_mode == "joystick":
                 joystick(js, motor_x, motor_y)
             else:
-                if not (model_inf.predict()):
-                    raise KeyboardInterrupt
+                results = model.track(img, imgsz=1024, classes=[0], persist=True, stream=True)
+                result = next(results)
+                img = result.plot()
+
+            # Calculate FPS
+            curr_time = time.time()
+            fps = 1 / (curr_time - prev_time) if prev_time else 0
+            prev_time = curr_time
+            put_text_rect(img, f'FPS: {fps:.2f}', (10, 30), 0.7, bg_color=(50, 50, 50))
+
+            cv2.imshow("DSLR Live", img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                print("Exiting program on 'q' key press")
+                raise KeyboardInterrupt
+
 
     except KeyboardInterrupt:
         pass
     finally:
+        print("Exiting program")
+        pg.quit()
+
+        print("Releasing camera")
+        cv2.destroyAllWindows()
+        cap.release()
+        
         print("Motor stopping")
         motor_x.stop_pwm()
         motor_y.stop_pwm()
