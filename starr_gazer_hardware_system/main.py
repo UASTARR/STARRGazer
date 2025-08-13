6290fa5760c83a013ac47c9ee50b8d07f0b30e80
Brian Lin <shihhsu1@ualberta.ca>
2025-08-12 18:54:34 -0600

    add: track ID

--- starr_gazer_hardware_system/main.py
+++ starr_gazer_hardware_system/main.py
@@ -90,7 +90,7 @@ def main():
             else:
                 results = model.track(img, imgsz=1024, classes=[0], persist=True, stream=True)
                 result = next(results)
-                print("Position: {result.boxes.xywh[0]}")
+                print(f"ID: {result.boxes.id[0]} Position: {result.boxes.xywh[0]}")
                 img = result.plot()
 
             # Calculate FPS
